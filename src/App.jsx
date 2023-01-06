9fe7af6d364ea3b1be98725e3cbd81c6ae63c6e6
Maxim <76053782+ggodos@users.noreply.github.com>
2023-01-06 17:26:42 +0500

    проба переписи

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,56 @@
-import { useState, Component } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import crossImg from './assets/x4.png'
-import circleImg from './assets/c1.png'
+import { useState, Component } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import crossImg from "./assets/x4.png";
+import circleImg from "./assets/c1.png";
 
-function reverseKeyValues(obj)
-{
+function reverseKeyValues(obj) {
   return Object.fromEntries(
-    Object
-      .entries(obj)
-      .map(([key, value]) => [value, key])
-  )
+    Object.entries(obj).map(([key, value]) => [value, key])
+  );
 }
 
 const playerFigureMap = {
-  first: 'circle',
-  second: 'cross'
-}
-const figurePlayerMap = reverseKeyValues(playerFigureMap)
+  first: "circle",
+  second: "cross",
+};
+const figurePlayerMap = reverseKeyValues(playerFigureMap);
 
-function getFigure(player)
-{
-  return playerFigureMap[player]
+function getFigure(player) {
+  return playerFigureMap[player];
 }
-function getPlayer(figure)
-{
-  return figurePlayerMap[figure]
+function getPlayer(figure) {
+  return figurePlayerMap[figure];
 }
 
-
-const imageSize = 32;
+const imageSize = 48;
+const selectButtonSize = 32;
 const imageMap = {
-  'circle': circleImg,
-  'cross': crossImg,
-}
+  circle: circleImg,
+  cross: crossImg,
+};
 
-function Cell(props)
-{
+function Cell(props) {
   return (
-    <div id={props.cellId} className='cell' onClick={props.onClick}>
+    <div id={props.cellId} className="cell" onClick={props.onClick}>
       <img src={props.imageSource} alt="" min-width={imageSize} />
     </div>
-  )
+  );
 }
 
-class Board extends Component
-{
-  renderCell(i)
-  {
-    const cells = this.props.cells
-    return <Cell
-      cellId={`cell-${i}`}
-      imageSource={cells[i].imageSource}
-      onClick={() => this.props.onClick(i)} />
+class Board extends Component {
+  renderCell(i) {
+    const cells = this.props.cells;
+    return (
+      <Cell
+        cellId={`cell-${i}`}
+        imageSource={cells[i].imageSource}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
   }
 
-  render()
-  {
+  render() {
     return (
       <div id="board">
         <div className="board-row">
@@ -75,88 +69,159 @@ class Board extends Component
           {this.renderCell(8)}
         </div>
       </div>
-    )
+    );
   }
 }
 
+function createPlayer(
+  playerName = "player",
+  playerFigure = null,
+  playerType = ""
+) {
+  return {
+    name: playerName,
+    canMakeTurn: false,
+    figure: playerFigure,
+    turnHandler: () => {},
+    type: playerType,
+  };
+}
 
-
-class App extends Component
-{
-  constructor(props)
-  {
-    super(props)
-    const gameCells = Array(9).fill().map((v, i) => ({
+function createCells() {
+  return Array(9)
+    .fill()
+    .map(() => ({
       value: null,
       used: false,
-      imageSource: ''
-    }))
+      imageSource: "",
+    }));
+}
+
+function GameRelated(props) {
+  if (props.gameStarted) {
+    return (
+      <div id="game-related">
+        <p>
+          Ходит: <span id="current-layer">{props.playerName}</span>
+        </p>
+        <Board cells={props.cells} onClick={props.clickHandleFunction}></Board>
+      </div>
+    );
+  } else {
+    return (
+      <div id="game-related">
+        <p>Выбери за кого играть</p>
+        <input
+          type="image"
+          src={circleImg}
+          id="circle-button"
+          className="choose-button"
+          width={selectButtonSize}
+          onClick={props.circleHandler}
+        />
+        <input
+          type="image"
+          src={crossImg}
+          id="cross-button"
+          className="choose-button"
+          width={selectButtonSize}
+          onClick={props.crossHandler}
+        />
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const gameCells = createCells();
 
     this.state = {
       cells: gameCells,
-      current: null,
-      turn: 0,
-      gameStarted: false
-    }
+      players: [],
+      currentPlayerIdx: 0,
+      gameStarted: false,
+    };
   }
-  startGame(settings)
-  {
+
+  render() {
+    const { gameStarted, currentPlayerIdx, players, cells } = this.state;
+
+    let playerName = "";
+    let handleFunction = () => {};
+    if (players.length > 0) {
+      const player = players[currentPlayerIdx];
+      playerName = players[currentPlayerIdx].name;
+      handleFunction = player.clickHandleFunction;
+    }
+
+    return (
+      <div className="App">
+        <GameRelated
+          circleHandler={() => this.startGame({ figure: "circle" })}
+          crossHandler={() => this.startGame({ figure: "cross" })}
+          gameStarted={gameStarted}
+          playerName={playerName}
+          cells={cells}
+          clickHandleFunction={handleFunction}
+        ></GameRelated>
+      </div>
+    );
+  }
+
+  startGame(settings) {
     const { figure } = settings;
-    document.getElementById('cross-button').disabled = true
-    document.getElementById('circle-button').disabled = true
-    this.setState(state => ({
-      current: figure,
-      turn: state.turn + 1,
-      gameStarted: true
-    }))
+    // document.getElementById("cross-button").disabled = true;
+    // document.getElementById("circle-button").disabled = true;
+
+    const human = createPlayer("Вы", figure, "human");
+
+    this.setState((state) => ({
+      gameStarted: true,
+    }));
   }
 
-
-  cellClickHandle(i)
-  {
-    const { cells, current } = this.state;
-    if (cells[i].used)
-    {
-      return
+  cellClickHandle(i) {
+    const { cells, currentPlayerIdx } = this.state;
+    if (cells[i].used) {
+      return;
     }
-    const newCells = cells.slice()
+    const newCells = cells.slice();
 
     newCells[i] = {
       value: current,
       imageSource: imageMap[current],
-      used: true
-    }
-    const newCurrent = current == getFigure('first') ? getFigure('second') : getFigure('first')
+      used: true,
+    };
+    const newCurrent =
+      current == getFigure("first") ? getFigure("second") : getFigure("first");
 
     this.setState({
       cells: newCells,
-      current: newCurrent
-    })
+      current: newCurrent,
+    });
 
-    const { winner, winCombination } = this.chooseWinner(newCells)
-    if (winner)
-    {
-      this.win(winner, winCombination)
+    const { winner, winCombination } = this.chooseWinner(newCells);
+    if (winner) {
+      this.win(winner, winCombination);
     }
   }
 
-  win(winner, winCombination)
-  {
+  win(winner, winCombination) {
     this.setState({
-      gameStarted: false
-    })
+      gameStarted: false,
+    });
     console.log(`Winner is ${winner}`);
     console.log(winCombination);
     console.log(this.state.cells);
-    winCombination.forEach(v =>
-    {
-      const el = document.getElementById(`cell-${v}`)
-      el.style.backgroundColor = "limegreen"
-    })
+    winCombination.forEach((v) => {
+      const el = document.getElementById(`cell-${v}`);
+      el.style.backgroundColor = "limegreen";
+    });
   }
 
-  chooseWinner(cells)
-  {
+  chooseWinner(cells) {
     const winCombinations = [
       [0, 1, 2],
       [3, 4, 5],
@@ -165,57 +230,28 @@ class App extends Component
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
-      [6, 4, 2]
-    ]
+      [6, 4, 2],
+    ];
 
-    let winner = undefined
-    let winCombination = undefined
-    winCombinations.forEach(combination =>
-    {
-      const firstCombinationFigure = cells[combination[0]].value
-      const winValid = combination.every((cellNumber) =>
-      {
-        const cellFigure = cells[cellNumber].value
+    let winner = undefined;
+    let winCombination = undefined;
+    winCombinations.forEach((combination) => {
+      const firstCombinationFigure = cells[combination[0]].value;
+      const winValid = combination.every((cellNumber) => {
+        const cellFigure = cells[cellNumber].value;
         // console.log(...combination, ...(combination.map(v => cells[v].value)));
-        if (cellFigure == null)
-        {
-          return false
+        if (cellFigure == null) {
+          return false;
         }
-        return cellFigure == firstCombinationFigure
-      })
-      if (winValid)
-      {
-        winner = getPlayer(firstCombinationFigure)
-        winCombination = combination
+        return cellFigure == firstCombinationFigure;
+      });
+      if (winValid) {
+        winner = getPlayer(firstCombinationFigure);
+        winCombination = combination;
       }
-    })
-    return { winner, winCombination }
-  }
-
-
-  render()
-  {
-    const { gameStarted } = this.state
-    const handleFunction = gameStarted ? i => this.cellClickHandle(i) : () => { }
-    return (
-      <div className="App">
-        <p>Выбери за кого играть</p>
-        <input type="image"
-          src={circleImg}
-          id='circle-button'
-          className='choose-button'
-          width={imageSize}
-          onClick={() => this.startGame({ figure: "circle" })} />
-        <input type="image"
-          src={crossImg}
-          id='cross-button'
-          className='choose-button'
-          width={imageSize}
-          onClick={() => this.startGame({ figure: "cross" })} />
-        <Board cells={this.state.cells} onClick={handleFunction}></Board>
-      </div >
-    )
+    });
+    return { winner, winCombination };
   }
 }
 
-export default App
+export default App;
