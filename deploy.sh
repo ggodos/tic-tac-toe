793397f0113288b2518421c45024a9767c6c4da5
Maxim <76053782+ggodos@users.noreply.github.com>
2023-01-09 19:51:39 +0500

    поправил очепятку

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ggods/tic-tac-toe.git main:gh-pages
+git push -f git@github.com:ggodos/tic-tac-toe.git main:gh-pages
 
 cd -
